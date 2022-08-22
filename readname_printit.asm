20b65677e010be7790c897aae588cd920f4b20d8
Crypt00o <octopus2672001@gmail.com>
2022-08-22 15:58:30 +0200

    added readname_printit.asm

---
+++ readname_printit.asm
@@ -0,0 +1,57 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;          readname_printname.asm                                                     ;
+;   [+] Hello Iam Crypt00o #                                                          ;
+; if you run this code it will ask you to enter your name and then greeting you like: ;
+;                                                                                     ;
+;[+] Please enter your name :    eslam mohamed                                        ;
+;[+] Crypt00o Greeting You , eslam mohamed                                            ;
+;                                                                                     ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ push 0x12b49b79
+ xor dword[esp],0x12345678
+ push 0xb0d231c9
+ push 0x31db31c0
+ push 0x3180cd01
+ push 0xb304b014
+ push 0xb2f989e4
+ push 0x7d8dd231
+ push 0xc931db31
+ push 0xc0311cc4
+ push 0x8380cde1
+ push 0x891db201
+ push 0xb304b020
+ push 0x5d2b5b68
+ push 0x70797243
+ push 0x686f3030
+ push 0x74686572
+ push 0x4720686e
+ push 0x69746568
+ push 0x6f592067
+ push 0x68202c20
+ push 0x756850d2
+ push 0x31c931db
+ push 0x31c03180
+ push 0xcd03b014
+ push 0xb2f989e4
+ push 0x7d8dd231
+ push 0xc931db31
+ push 0xc03121c4
+ push 0x8380cd01
+ push 0xb304b021
+ push 0xb2e18920
+ push 0x5d2b5b68
+ push 0x61656c50
+ push 0x68652065
+ push 0x73687265
+ push 0x746e6875
+ push 0x6f792068
+ push 0x616e2072
+ push 0x683a2065
+ push 0x6d682020
+ push 0x20206850
+ push 0xffe48320
+ push 0xec83e589
+ push 0xd231c931
+ push 0xdb31c031
+ call esp
